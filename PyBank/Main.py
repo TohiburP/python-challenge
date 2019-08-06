6f83b7b982402cfe6379415d05e8ff3488a5f9b1
TohiburP <51770412+TohiburP@users.noreply.github.com>
2019-08-05 23:05:06 -0400

    Add files via upload

--- PyBank/Main.py
+++ PyBank/Main.py
@@ -1,11 +1,9 @@
 import os
 import csv
 
-#path to the csvfile
-
 csvpath = os.path.join('budget_data.csv')
 
-#initializing the variables 
+#the variables 
 total_months = 0
 total_revenue =0
 changes =[]
@@ -20,7 +18,8 @@ with open(csvpath, newline = '') as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ',')
     next(csvreader, None)
     row = next(csvreader)
-# calculating the total number of months and total revenue
+
+#the total number of months and total revenue
     previous_profit = int(row[1])
     total_months = total_months + 1
     total_revenue = total_revenue + int(row[1])
@@ -32,29 +31,29 @@ with open(csvpath, newline = '') as csvfile:
         total_months = total_months + 1
         total_revenue = total_revenue + int(row[1])
 
-        # Calculate change from this month to previous months
+        #changes from this month to previous months
         change = int(row[1]) - previous_profit
         changes.append(change)
         previous_profit = int(row[1])
         date_count.append(row[0])
         
-        #calculating the greatest increase
+        #the greatest increase
         if int(row[1]) > greatest_inc:
             greatest_inc = int(row[1])
             greatest_inc_month = row[0]
             
-        #calculating the greatest decrease
+        #cthe greatest decrease
         if int(row[1]) < greatest_dec:
             greatest_dec = int(row[1])
             greatest_dec_month = row[0]  
       
-    # calculating the average and date
+    # summing the average and date
     average_change = sum(changes)/len(changes)
 
     high = max(changes)
     low = min(changes)
 
-    # printing all values
+    # print values
     print("Financial Analysis")
     print("Total Months:" + str(total_months))
     print("Total Amount:" + str(total_revenue))
